9348232fe7fe830e9034f4472550b7119b4d690b
Brayan Carreño <brayan26camilo26@gmail.com>
2021-09-22 18:51:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lightson/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.lightson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Registrarse (View view){
+        Intent registro = new Intent(this, RegistrarCuenta.class);
+        startActivity(registro);
+    }
+    public void IniciarSesion (View view){
+        Intent inicio = new Intent(this, RegistrarCuenta.class);
+        startActivity(inicio);
+    }
+}
